ec792b2df4a28d9027c1d0043b018bb09bf4a631
Ulrich <100772767+ugroh@users.noreply.github.com>
2025-01-27 17:34:57 +0100

    Update

---
+++ python/test-claude.py
@@ -0,0 +1,18 @@
+import os
+from anthropic import Anthropic
+
+client = Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_KEY"),  # This is the default and can be omitted
+)
+
+message = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello, Claude",
+        }
+    ],
+    model="claude-3-5-sonnet-latest",
+)
+print(message.content)
